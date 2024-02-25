aad3739136c41b020a2ae3d1a269cd5268aefc87
ryota <kurokawa.ryota.1124@gmail.com>
2024-02-25 18:45:01 +0900

    initialization

---
+++ XcodegenTest/XcodegenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodegenTestApp.swift
+//  XcodegenTest
+//
+//  Created by 黒川良太 on 2024/02/25.
+//
+
+import SwiftUI
+
+@main
+struct XcodegenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
